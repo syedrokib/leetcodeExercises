d8782918c860683f51d075682466d4227bf1ff87
Syed Rokib <syed.rokib@bell.ca>
2019-06-17 15:48:18 -0400

    Convert expression string to separated string array of fractions

--- src/main/java/problems/medium/FractionAddition.java
+++ src/main/java/problems/medium/FractionAddition.java
@@ -1,8 +1,43 @@
 package problems.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //https://leetcode.com/problems/fraction-addition-and-subtraction/
-public class FractionAddition {
-    public static String fractionAddition(String expression) {
+class FractionAddition {
+    static String fractionAddition(String expression) {
+
+        //TODO: expression does not start with -
+        if (!expression.startsWith("-")) expression = "+" + expression;
+
+        List<String> fractions = new ArrayList<>();
+
+        for (String token : expression.split("[-+]")) {
+
+            if ("".equals(token)) continue;
+
+            final String positiveToken = "+" + token;
+            final String negativeToken = "-" + token;
+
+            if (expression.contains(positiveToken)) {
+                expression = expression.replace(positiveToken, "");
+                fractions.add(positiveToken.replace("+", ""));
+            } else {
+                expression = expression.replace(negativeToken, "");
+                fractions.add(negativeToken);
+            }
+
+
+//            System.out.println(token);
+        }
+
+        System.out.println(fractions);
+
+
+//        expression = expression.replace("+", "");
+//        System.out.println(expression);
+
+
         return null;
     }
 
--- src/test/java/problems/medium/FractionAdditionTest.java
+++ src/test/java/problems/medium/FractionAdditionTest.java
@@ -9,6 +9,7 @@ public class FractionAdditionTest {
 
     @Test
     public void test() {
-        assertNull(fractionAddition(""));
+        assertNull(fractionAddition("-1/2+1/2-55/9-9/5"));
+        assertNull(fractionAddition("9/5-1/2+1/2-55/9-9/5"));
     }
 }

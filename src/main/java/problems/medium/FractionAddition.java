78782278ad53c98c9908237c805f74af22e8e591
Syed Rokib <syed.rokib@bell.ca>
2019-06-18 17:17:59 -0400

    Using one array to: 1. collect fractions from expression 2. readjust fractions by multiplying numerator and denominator with common factor 3. calculate sum of numerators of fractions that now all have the same denominator

--- src/main/java/problems/medium/FractionAddition.java
+++ src/main/java/problems/medium/FractionAddition.java
@@ -15,51 +15,47 @@ class FractionAddition {
 
     static String fractionAddition(String expression) {
 
-        if (!expression.startsWith(MINUS_SIGN)) expression = PLUS_SIGN + expression;
+        if (startsWithPositiveFraction(expression)) expression = PLUS_SIGN + expression;
 
-        List<String> fractions = new ArrayList<>();
+        List<int[]> fractions = new ArrayList<>();
         Matcher m = Pattern.compile("[-+][\\d]+[/][\\d]+").matcher(expression);
-        while (m.find()) fractions.add(m.group());
-
-        int commonDenominator = 1;
-
-        for (String fraction : fractions) {
-            final String denominator = fraction.split("[/]")[1];
-            commonDenominator *= Integer.parseInt(denominator);
+        while (m.find()) {
+            String[] tokens = m.group().split("/");
+            int numerator = Integer.parseInt(tokens[0]);
+            int denominator = Integer.parseInt(tokens[1]);
+            fractions.add(new int[]{numerator, denominator});
         }
 
-        List<String> unifiedFractions = new ArrayList<>();
+        int commonDenominator = 1;
+        for (int[] fraction : fractions) commonDenominator *= fraction[1];
 
-        for (String fraction : fractions) {
-            String numerator = fraction.split("[/]")[0];
-            String denominator = fraction.split("[/]")[1];
+        for (int[] fraction : fractions) {
 
-            int ratio = commonDenominator / Integer.valueOf(denominator);
-
-            if (numerator.startsWith(MINUS_SIGN)) {
-                unifiedFractions.add(MINUS_SIGN + Integer.parseInt(numerator.replace(MINUS_SIGN, "")) * ratio + "/" + commonDenominator);
-            } else {
-                unifiedFractions.add(Integer.parseInt(numerator) * ratio + "/" + commonDenominator);
-            }
+            int ratio = commonDenominator / fraction[1];
+            fraction[0] *= ratio;
+            fraction[1] = commonDenominator;
         }
 
         int numeratorSum = 0;
-        for (String fraction : unifiedFractions) {
+        for (int[] fraction : fractions) {
 
-            String numerator = fraction.split("[/]")[0];
+            numeratorSum += fraction[0];
 
-            if (numerator.startsWith(MINUS_SIGN)) numeratorSum -= Integer.parseInt(numerator.replace(MINUS_SIGN, ""));
-            else numeratorSum += Integer.parseInt(numerator);
         }
 
         int reductionFactor = 1;
-        for (int i = 1; i <= max(commonDenominator, numeratorSum); i++)
+        for (int i = 1; i <= max(commonDenominator, numeratorSum); i++) {
             if (commonDenominator % i == 0 && numeratorSum % i == 0) reductionFactor = i;
+        }
 
         numeratorSum /= reductionFactor;
         commonDenominator /= reductionFactor;
 
         return numeratorSum + "/" + commonDenominator;
+    }
+
+    private static boolean startsWithPositiveFraction(String expression) {
+        return !expression.startsWith(MINUS_SIGN);
     }
 
 }

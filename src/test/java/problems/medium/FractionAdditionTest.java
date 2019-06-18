f36db6a45051e61bfc099b5be063792512f7c4e9
Syed Rokib <syed.rokib@bell.ca>
2019-06-17 22:54:14 -0400

    Add tests for zero and positive answers

--- src/test/java/problems/medium/FractionAdditionTest.java
+++ src/test/java/problems/medium/FractionAdditionTest.java
@@ -2,7 +2,7 @@ package problems.medium;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static problems.medium.FractionAddition.fractionAddition;
 
 public class FractionAdditionTest {
@@ -13,8 +13,18 @@ public class FractionAdditionTest {
         assertEquals("-55/9", fractionAddition("9/5-1/2+1/2-55/9-9/5"));
     }
 
-//    @Test
-//    public void zero() {
-//        assertEquals("0/1", "-1/2+1/2");
-//    }
+    @Test
+    public void zero() {
+        assertEquals("0/1", fractionAddition("-1/2+1/2"));
+        assertEquals("0/1", fractionAddition("1/2-1/2"));
+        assertEquals("0/1", fractionAddition("0/6-0/44"));
+        assertEquals("0/1", fractionAddition("-0/6-0/44+0/444"));
+        assertEquals("1/1", fractionAddition("-0/6-0/44+0/444+5/5"));
+    }
+
+    @Test
+    public void positiveAnswers() {
+        assertEquals("1/1", fractionAddition("-0/6-0/44+0/444+5/5"));
+        assertEquals("149/148", fractionAddition("3/444+5/5"));
+    }
 }

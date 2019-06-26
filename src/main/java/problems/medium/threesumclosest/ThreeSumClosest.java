58c9282f45d9a89a66521a3b3fb94aacadbaeea9
Syed Rokib <syed.rokib@bell.ca>
2019-06-26 14:46:20 -0400

    If there aren't 3 different numbers in the array that add to the given target, don't continue, try to find two others to get as close as possible Skip invalid combo (reuse of array index) right away

--- src/main/java/problems/medium/threesumclosest/ThreeSumClosest.java
+++ src/main/java/problems/medium/threesumclosest/ThreeSumClosest.java
@@ -3,56 +3,45 @@ package problems.medium.threesumclosest;
 import java.util.HashMap;
 import java.util.Map;
 
-//https://leetcode.com/problems/fraction-addition-and-subtraction/
+//https://leetcode.com/problems/3sum-closest/
 class ThreeSumClosest {
 
     static int threeSumClosest(int[] nums, int target) {
 
-        // map sum of pairs -> to pairs
-
-        HashMap<Integer, int[]> map = new HashMap<>();
+        HashMap<Integer, int[]> sumsOfPairs = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                map.put(nums[i] + nums[j], new int[]{nums[i], nums[j], i, j});
+                sumsOfPairs.put(nums[i] + nums[j], new int[]{nums[i], nums[j], i, j});
             }
         }
 
-        int min = Integer.MAX_VALUE;
-        int total = 0;
+        int champion = Integer.MAX_VALUE;
+        int totalOfChampion = 0;
 
         for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int requiredDifference = target - num;
+            if (sumsOfPairs.containsKey(requiredDifference) && !isIndexReused(i, sumsOfPairs.get(requiredDifference)))
+                return target;   // True -> there exists 3 different numbers in the array that add to the given target.
 
-            //one pass to see if nums has 3 numbers that add to target
-            //indicates that there exists two numbers with sum as key
-            //that can be added to i to form an array
-            int thisnum = nums[i];
-            int diff = target - thisnum;
-            if (map.containsKey(diff)) {
-                int[] ints = map.get(diff);
-//                System.out.println();
-
-                // check for repetition
-                if (ints[2] != i && ints[3] != i) {
-//                    System.out.println();
-                    return target;
-                }
-            }
-
-            for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
-
-                Integer thiskey = entry.getKey();
-                int[] value = entry.getValue();
-
-                int targetDiff = Math.abs(target - (thisnum + thiskey));
-
-                if (targetDiff < min && value[2] != i && value[3] != i) {
-                    min = targetDiff;
-                    total = thisnum + thiskey;
+            for (Map.Entry<Integer, int[]> entry : sumsOfPairs.entrySet()) {
+                // Try and find two other numbers that can be used in conjunction with this number to get as close as possible to the target.
+                if (isIndexReused(i, entry.getValue())) continue;   // Skip invalid combination.
+                int sum = entry.getKey();
+                int challenger = Math.abs(target - (nums[i] + sum));
+                if (challenger < champion) {
+                    champion = challenger;
+                    totalOfChampion = sum + nums[i];
                 }
             }
         }
-        return total;
+        return totalOfChampion;
     }
 
+    private static boolean isIndexReused(int index, int[] pair) {
+        int firstIndex = pair[2];
+        int secondIndex = pair[3];
+        return firstIndex == index || secondIndex == index;
+    }
 }

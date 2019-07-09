b163558905d9281e2e1bb467c748a84435ae9a8c
syedrokib <syedrokib@gmail.com>
2019-07-08 23:58:17 -0400

    Iterate grid to determine starting row and column for DFS Prepare recursive function

--- src/main/java/problems/easy/islandperimeter/IslandPerimeter.java
+++ src/main/java/problems/easy/islandperimeter/IslandPerimeter.java
@@ -2,6 +2,33 @@ package problems.easy.islandperimeter;
 
 public class IslandPerimeter {
     public int islandPerimeter(int[][] grid) {
+
+        // you add 3 except for the side that you share
+
+        int startingRow = -1;
+        int startingColumn = -1;
+
+
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[0].length; column++) {
+                if (grid[row][column] == 1) {
+                    startingRow = row;
+                    startingColumn = column;
+                    break;
+                }
+            }
+        }
+
+        return findPerimeter(grid, startingRow, startingColumn, 4);
+
+
+
+
+
+    }
+
+    private int findPerimeter(int[][] grid, int startingRow, int startingColumn, int totalPerimeter) {
+
         return 0;
     }
 

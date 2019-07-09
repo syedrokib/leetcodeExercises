559a216858d454061337eccbb9ab004d153f893b
syedrokib <syedrokib@gmail.com>
2019-07-09 00:14:37 -0400

    Keep track of visited nodes

--- src/main/java/problems/easy/islandperimeter/IslandPerimeter.java
+++ src/main/java/problems/easy/islandperimeter/IslandPerimeter.java
@@ -8,26 +8,36 @@ public class IslandPerimeter {
         int startingRow = -1;
         int startingColumn = -1;
 
+        boolean isStartDetermined = false;
+        int gridRowLength = grid.length;
+        int gridColumnLength = grid[0].length;
 
-        for (int row = 0; row < grid.length; row++) {
-            for (int column = 0; column < grid[0].length; column++) {
-                if (grid[row][column] == 1) {
+        boolean[][] visitedGrid = new boolean[gridRowLength][gridColumnLength];
+
+        for (int row = 0; row < gridRowLength; row++) {
+            for (int column = 0; column < gridColumnLength; column++) {
+                visitedGrid[row][column] = false;
+                if (!isStartDetermined && grid[row][column] == 1) {
                     startingRow = row;
                     startingColumn = column;
-                    break;
+                    isStartDetermined = true;
                 }
             }
         }
 
-        return findPerimeter(grid, startingRow, startingColumn, 4);
-
-
-
-
+        return findPerimeter(grid, startingRow, startingColumn, 4, visitedGrid);
 
     }
 
-    private int findPerimeter(int[][] grid, int startingRow, int startingColumn, int totalPerimeter) {
+    private int findPerimeter(int[][] grid, int startingRow, int startingColumn, int totalPerimeter,
+                              boolean[][] visitedGrid) {
+
+        //keep track of visited nodes
+
+        boolean hasLeft = (startingColumn - 1 >= 0) && (grid[startingRow][startingColumn - 1] == 1);
+        boolean hasRight = (startingColumn + 1 < grid[0].length) && (grid[startingRow][startingColumn + 1] == 1);
+        boolean hasTop = (startingRow - 1 >= 0) && (grid[startingRow - 1][startingColumn] == 1);
+        boolean hasBottom = (startingRow + 1 < grid.length) && (grid[startingRow + 1][startingColumn] == 1);
 
         return 0;
     }

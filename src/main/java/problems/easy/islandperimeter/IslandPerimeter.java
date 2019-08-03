306fce8890824978557ed4b810f1e77f8a81a0dd
syedrokib <syedrokib@gmail.com>
2019-08-03 13:18:56 -0400

    Recognize that perimeter increases by 2 upon discovery of a neighbour

--- src/main/java/problems/easy/islandperimeter/IslandPerimeter.java
+++ src/main/java/problems/easy/islandperimeter/IslandPerimeter.java
@@ -32,12 +32,19 @@ public class IslandPerimeter {
     private int findPerimeter(int[][] grid, int startingRow, int startingColumn, int totalPerimeter,
                               boolean[][] visitedGrid) {
 
+        visitedGrid[startingRow][startingColumn] = true;
+
         //keep track of visited nodes
 
-        boolean hasLeft = (startingColumn - 1 >= 0) && (grid[startingRow][startingColumn - 1] == 1);
-        boolean hasRight = (startingColumn + 1 < grid[0].length) && (grid[startingRow][startingColumn + 1] == 1);
-        boolean hasTop = (startingRow - 1 >= 0) && (grid[startingRow - 1][startingColumn] == 1);
-        boolean hasBottom = (startingRow + 1 < grid.length) && (grid[startingRow + 1][startingColumn] == 1);
+        //check bounds && check for node existence && check for not visited
+
+
+        boolean hasLeft = (startingColumn - 1 >= 0) && (!visitedGrid[startingRow][startingColumn - 1]) && (grid[startingRow][startingColumn - 1] == 1);
+        boolean hasRight = (startingColumn + 1 < grid[0].length) && (!visitedGrid[startingRow][startingColumn + 1]) && (grid[startingRow][startingColumn + 1] == 1);
+        boolean hasTop = (startingRow - 1 >= 0) && (!visitedGrid[startingRow - 1][startingColumn]) && (grid[startingRow - 1][startingColumn] == 1);
+        boolean hasBottom = (startingRow + 1 < grid.length) && (!visitedGrid[startingRow - 1][startingColumn]) && (grid[startingRow - 1][startingColumn] == 1);
+
+        if (hasLeft) totalPerimeter = findPerimeter(grid, startingRow, startingColumn - 1, 2 + totalPerimeter, visitedGrid);
 
         return 0;
     }
